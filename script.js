dc691c5e825134018ecd1dd6aa1681d35d78bea5
Ilakya <ilakyasivasamy@gmail.com>
2023-04-10 11:24:14 +0530

    added contact.html and made related changes

--- script.js
+++ script.js
@@ -37,3 +37,40 @@ var MainImg= document.getElementById("MainImg");
         smallImg[3].onclick=function(){
             MainImg.src=smallImg[3].src;
         }
+
+
+
+/*----------------------------google sheet------------------------*/
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw45ehzl-Ncpn6C6DGPR5yj7JBc3K6i_gCpSEp2WXmWM6bgqvNIEKCYaW9fTp8SS4vxZQ/exec'
+			const form = document.forms['GoogleformName']
+		  
+			form.addEventListener('submit', e => {
+			  e.preventDefault()
+			  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+              .then(() => {
+                alert("Success!");
+              })
+             .catch(error => console.error('Error!', error.message))
+			})  
+
+
+
+           /*window.addEventListener("load", function() {
+                const form = document.getElementById('g');
+                form.addEventListener("submit", function(e) {
+                  e.preventDefault();
+                  const data = new FormData(form);
+                  const action = e.target.action;
+                  fetch(action, {
+                    method: 'POST',
+                    body: data,
+                  })
+                  .then(() => {
+                    alert("Success!");
+                  })
+                });
+              });  */
+              
+            
+              
